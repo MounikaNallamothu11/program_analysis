a4261010dc9517ca84835fc6072c71fc2651ef35
Victornrib <victornrib@gmail.com>
2024-11-25 21:44:14 +0100

    changing main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from change_detector import ChangeDetector
+from static_analysis.change_detector import ChangeDetector
 from static_analysis.dependency_tracker import DependencyTracker
 
 
@@ -18,24 +18,42 @@ def main():
 
     # ChangeDetector is a class that detects changes in Java code
     detector = ChangeDetector()
-    modified_methods, new_methods, removed_methods = detector.detect_changes_in_java_code(printer=True, showBodies=False)
+    changes = detector.detect_changes(printer=True, showBodies=False)
+
+    modified_methods = changes["modified_methods"]
+    added_methods = changes["added_methods"]
+    removed_methods = changes["removed_methods"]
+    modified_tests = changes["modified_tests"]
+    added_tests = changes["added_tests"]
+    removed_tests = changes["removed_tests"]
+
 
     # Combine directly affected methods (modified and removed)
     directly_affected_methods = modified_methods | removed_methods
 
+    # Combine directly affected tests (modified and removed)
+    directly_affected_tests = modified_tests | removed_tests
+
+
     if directly_affected_methods:
         print(f"\nDirectly affected methods: {directly_affected_methods}")
-    else:
-        print("\nNo methods were directly affected. No need to run the dependency tracker.")
+    # else:
+    #     print("\nNo methods were directly affected, thus no tests should be rerun.")
+
+    if directly_affected_tests:
+        print(f"\nDirectly affected tests: {directly_affected_tests}")
         
-    if new_methods:
-        print(f"\nNew tests should be made for the following methods: {new_methods}")
+    if added_methods:
+        print(f"\nNew tests should be made for the following methods: {added_methods}")
+
+    if added_tests:
+        print(f"\nNew tests were added but still haven't run: {added_tests}")
+        
 
     # DependencyTracker is a class that creates an AST of a Java file to track all caller methods of a given list of methods
     dependencyTracker = DependencyTracker()
 
     # Get all caller methods for the directly affected methods
-    #indirectly_affected_methods = dependencyTracker.provide_all_caller_methods(directly_affected_methods)
     indirectly_affected_methods = dependencyTracker.provide_all_caller_methods(directly_affected_methods)
 
     print(f"\nIndirectly affected methods: {indirectly_affected_methods}\n")
