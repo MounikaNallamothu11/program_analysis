089c2c5261b3df13ce4ee4be7694e6987f6493b0
Chukwukaodinaka <emmanuelcsimon2002@gmail.com>
2024-11-27 20:51:22 +0100

    remove logs

--- java/modified/test/BankAccountTest.java
+++ java/modified/test/BankAccountTest.java
@@ -8,8 +8,6 @@ public class BankAccountTest {
 
     @Test
     public void testDeposit() {
-System.out.println("CALL testDeposit");
-System.out.println("CALL testDeposit");
         BankAccount account = new BankAccount(1, 100);
         account.deposit(50);
         assertEquals(150, account.getBalance());
@@ -17,8 +15,6 @@ System.out.println("CALL testDeposit");
 
     @Test
     public void testTransferSuccess() {
-System.out.println("CALL testTransferSuccess");
-System.out.println("CALL testTransferSuccess");
         BankAccount sourceAccount = new BankAccount(1, 100);
         BankAccount destinationAccount = new BankAccount(2, 50);
         String result = sourceAccount.transfer(50, destinationAccount);
@@ -28,8 +24,6 @@ System.out.println("CALL testTransferSuccess");
 
     @Test
     public void testTransferInsufficientFunds() {
-System.out.println("CALL testTransferInsufficientFunds");
-System.out.println("CALL testTransferInsufficientFunds");
         BankAccount sourceAccount = new BankAccount(1, 100);
         BankAccount destinationAccount = new BankAccount(2, 50);
         String result = sourceAccount.transfer(150, destinationAccount);
@@ -39,16 +33,12 @@ System.out.println("CALL testTransferInsufficientFunds");
 
     @Test
     public void testCalculateInterestDivideByZero() {
-System.out.println("CALL testCalculateInterestDivideByZero");
-System.out.println("CALL testCalculateInterestDivideByZero");
         BankAccount account = new BankAccount(1, 100);
         assertThrows(ArithmeticException.class, () -> account.calculateInterest(0));
     }
 
     @Test
     public void testSumPositiveBalances() {
-System.out.println("CALL testSumPositiveBalances");
-System.out.println("CALL testSumPositiveBalances");
         List<BankAccount> accounts = new ArrayList<>();
         accounts.add(new BankAccount(1, 100)); // Positive balance
         accounts.add(new BankAccount(2, -50)); // Negative balance
@@ -61,8 +51,6 @@ System.out.println("CALL testSumPositiveBalances");
 
     @Test
     public void testGetAccountSummary() {
-System.out.println("CALL testGetAccountSummary");
-System.out.println("CALL testGetAccountSummary");
         BankAccount account = new BankAccount(1, 100);
         String summary = account.getAccountSummary();
         assertEquals("Account 1 has a balance of 100.0", summary);
